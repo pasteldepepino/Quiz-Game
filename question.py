7087e5428702146f736e61f8d1a0b4d4b7e9377f
Raul Graeff <pasteldepepino.gamer@gmail.com>
2025-06-17 11:32:55 -0300

    Update question.py
    Fixed the loop inefficient method in user_begin_prompt function.

--- question.py
+++ question.py
@@ -68,13 +68,13 @@ def play_quiz():
 def user_begin_prompt():
     print("Wanna test your GK?\nA. Yes\nB. No")
     play = input()
+    while play != 'a' or play != 'b' or play != 'y':
+        print("Hmm. I didn't quite understand that.\nPress A to play, or B to quit.")
+        play - input()
     if play.lower() == 'a' or play.lower() ==  'y':
         play_quiz()
     elif play.lower() == 'b':
         print("Hope you come back soon!")
-    else:
-        print("Hmm. I didn't quite understand that.\nPress A to play, or B to quit.")
-        user_begin_prompt()
         
 def execute():
     user_begin_prompt()
